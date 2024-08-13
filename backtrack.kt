d29aede5cd2e0dcbaf5842331bbb5111d7cd77a7
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-13 12:43:23 +0530

    .

---
+++ backtrack.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>) {
+    println("Hello, world!!!")
+    var a=10
+    println(a)
+    var `try` = 40
+    println(`try`)
+}
---
+++ var.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Hello, world!!!")
+    var a=10
+    println(a)
+    var t=40
+    println(t)
+}
