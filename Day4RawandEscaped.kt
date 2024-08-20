21f40f2da6011cfc16507f0a127d084f0351b642
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-20 12:28:09 +0530

    .

---
+++ Day4RawandEscaped.kt
@@ -0,0 +1,8 @@
+fun  main(args: Array<String>) {
+    val x:String="I am the man ok man what is man\n" // Escaped String
+    val y:String=""" am the man ok man what is man   
+                    happy world hai ki nahi kya kar rahe ho""" // Raw String
+                    println(x)
+                    println(y)
+
+}
