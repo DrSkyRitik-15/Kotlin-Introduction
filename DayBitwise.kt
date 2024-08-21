65676945ef84bd5ea3c5b7f16ad349475ee33b53
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-21 12:45:36 +0530

    .

---
+++ DayBitwise.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val e=5 //0101 in binary
+    val f=3
+    val andBitwise=e and f
+    println("bitwise  + "andBitwise)
+
+
+
+    // range
+    val range=1..10
+    for (i in range){
+        println(i)
+        }
+        val range2=1 until 10
+        for (i in range2){
+            println(i)
+            }
+            val range3=1..10 step 2
+            for (i in range3){
+                println(i)
+                }
+                    
+}
---
+++ program1.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val totalAmount=270;
+    val membership=true;
+    if(totalAmount>=1000 && membership)
+    {
+        println("Customer having 20% discount")
+    }
+    else if(totalAmount<=1000 && membership)
+    {
+        println("Customer having 10% discount")
+    }
+    else
+    {
+        println("Customer having no discount")
+    }
+}
