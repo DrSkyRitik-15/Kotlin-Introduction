c47ed402cc6467c6ffc47379faf55b11e79b4156
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-26 12:58:03 +0530

    12

--- Program.kt
+++ Program.kt
@@ -2,8 +2,15 @@ fun temp(a:Int)
 {
     val F = (9/5 * a) + 32
     println("Temperature in Fahrenheit is $F")
+    
+}
+fun changeMore(d:Int)
+{
+     val c:Float = 5/9*(d-32).
+     println("The value is $c")
 }
 fun main(){
     temp(0)
     temp(30);
+    changeMore(50)
 }
