969abbe01b1f9395ebf29014abfdf03321685d84
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-28 12:22:50 +0530

    class

---
+++ Day8CLass.kt
@@ -0,0 +1,23 @@
+fun main()
+{
+    val a=Car(name="Ritik",type="Petrol",kmRun=100)
+    val b=Car(name="Garima",type="Gng",kmRun=200)
+
+    println(a.name)
+    println(a.type)
+    println(b.name)
+    println(b.type)
+
+    a.driveCar();
+    b.applyBreaks();
+
+}
+class Car(val name:String,val type:String,var kmRun:Int)
+{
+    fun driveCar()  {
+          println("$name Car id driving")
+    }                                               // method
+    fun applyBreaks(){
+        println("$name Car is applying breaks")
+    }
+}
