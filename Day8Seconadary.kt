faa3ab05e9f942eafc57e1e86f468c1ab7c77c9f
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-08-28 12:54:48 +0530

    12

---
+++ Day8Seconadary.kt
@@ -0,0 +1,14 @@
+fun main(){
+    Apple(5,6);
+
+}
+//class with one secondary constructor
+
+class Apple{
+    constructor(var a:Int,  var b:Int)
+    {
+        val c=a+b
+        println("The sum is $a and $b = $c");
+
+    }
+}
